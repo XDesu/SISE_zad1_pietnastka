51f72a5a77c199297b565cddff573b96eb544c4a
Adam Kapuściński <ziemniaczek320@gmail.com>
2021-10-11 16:45:42 +0200

    początek prac na readerem zagadem; utworzenie gitignore

---
+++ main.py
@@ -0,0 +1,8 @@
+from puzzle_reader import PuzzleReader
+
+def main():
+    PuzzleReader('./puzzles/1.txt')
+
+
+if __name__ == "__main__":
+    main()
---
+++ puzzle_reader.py
@@ -0,0 +1,34 @@
+
+class PuzzleReader():
+
+    def __init__(self, file_name) -> None:
+        f = open(file_name, 'r')
+
+        # get width and height of puzzle from file
+        self.width, self.height = f.readline().split()
+        self.width, self.height = int(self.width), int(self.height)
+        
+        self.tab = []
+
+        # read puzzle from file
+        i: int = 0
+        while line := f.readline():
+            line = list(map(int, line.split()))
+            self.tab.append(line)
+            if len(self.tab[i]) != self.width:
+                f.close()
+                raise Exception(f'Wrong amount of numbers in line {i+2} in file {file_name}')
+            i = i+1
+        f.close()
+
+        if len(self.tab) != self.height:
+            raise Exception(f'Wrong amount of lines in file {file_name}')
+
+    def getHeight(self) -> int:
+        return self.height
+
+    def getWitdth(self) -> int:
+        return self.width
+        
+    def getArray(self):
+        return self.tab
